7456d0d19d8082e9cb950a2190cb06084d29f358
sonnemaf <fons.sonnemans@reflectionit.nl>
2022-01-28 11:23:04 +0100

    Add project files.

---
+++ App55/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using App55.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App55 {
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page {
+
+        private List<Employee> Employees { get; } = new List<Employee> {
+                new Employee("Mike", 2000),
+                new Employee("Steve", 4000),
+                new Employee("Anna", 3000),
+            };
+
+        public MainPage() {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ App55/Models/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App55.Models {
+
+    public class EmployeeList : List<Employee> {
+
+    }
+
+    public class Employee {
+        public Employee() {
+        }
+
+        public Employee(string name, double salary) {
+            this.Name = name;
+            this.Salary = salary;
+        }
+
+        public string Name { get; set; }
+
+        public void RaiseSalary(double percentage) {
+            this.Salary += Salary * (percentage / 100);
+        }
+
+        public double Salary { get; set; }
+
+        public override string ToString() {
+            return string.Format("Employee Name = {0}, Salary = {1}", this.Name, this.Salary);
+        }
+    }
+
+}
